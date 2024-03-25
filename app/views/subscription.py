a4114ae7d7f6c2947fb173c298a2122bd0b8cbb1
DummyTummy123 <hiren@magentaconnect.com>
2024-03-24 21:54:17 -0400

    Added stripe webhooks

--- app/views/subscription.py
+++ app/views/subscription.py
@@ -1,4 +1,4 @@
-from flask import request, redirect
+from flask import request, redirect, jsonify
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from app import app, db, stripe
 from app.models import User
@@ -61,42 +61,57 @@ def set_payment():
     if not user:
         return APIResponse.error("Couldn't find a user account", 400)
     
-    if not user.stripe_cus_id:
+    try:
+        kk = stripe.Customer.retrieve(user.stripe_cus_id)
+        if "deleted" in kk:
+            assert kk["deleted"] == False
+    except:
         tmpcus = stripe.Customer.create(email=user.email, name=f"{user.fn} {user.ln}")
         user.stripe_cus_id = tmpcus["id"]
         db.session.commit()
     
     itt = list(plan.keys())[0]
     ppi = search_for_price(int(itt), plan[itt]["s"], plan[itt]["d"], team=plan[itt].get("t"))
-    if not user.stripe_sub_id:
-        checkout_session = stripe.checkout.Session.create(
-            customer=user.stripe_cus_id,
-            line_items=[
-                {
-                    'price': ppi, "quantity": 1
-                },
-            ],
-            metadata = {"user_id": current_user, "ins_type": itt, "students": plan[itt]["s"], "duration": plan[itt]["d"], "team": plan[itt].get("t")},
-            mode='subscription',
-            success_url = app.config.get("BACKEND_URL") + '/call/payment?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url = app.config.get("BACKEND_URL") + '/cancel.html',
-        )
-        return redirect(checkout_session.url)
-        # return jsonify(checkout_session.url)
-    else:
-        sub = stripe.Subscription.retrieve(user.stripe_sub_id)
-        sub_item_id = dict(sub)["items"]["data"][0]["id"]
-        stripe.SubscriptionItem.modify(sub_item_id, price = ppi)
-        return APIResponse.success("Plan updated successfully", 200)
+
+    if user.stripe_sub_id:
+        try:
+            sub = stripe.Subscription.retrieve(user.stripe_sub_id)
+            sub_item_id = dict(sub)["items"]["data"][0]["id"]
+            stripe.SubscriptionItem.modify(sub_item_id, price = ppi, quantity = int(plan[itt]["c"]))
+            sub = stripe.Subscription.retrieve(user.stripe_sub_id)
+            if sub.status == "active":
+                return APIResponse.success("Plan updated successfully", 200)
+            elif sub.status == "past_due":
+                return stripe.Invoice.retrieve(sub.latest_invoice).hosted_invoice_url
+                return redirect(stripe.Invoice.retrieve(sub.latest_invoice).hosted_invoice_url)
+        except:
+            pass
+
+    checkout_session = stripe.checkout.Session.create(
+        customer=user.stripe_cus_id,
+        line_items=[
+            {
+                'price': ppi, "quantity": int(plan[itt]["c"])
+            },
+        ],
+        metadata = {"user_id": current_user, "ins_type": itt, "students": plan[itt]["s"], "duration": plan[itt]["d"], "team": plan[itt].get("t"), "count": plan[itt]["c"]},
+        mode='subscription',
+        success_url = app.config.get("BACKEND_URL") + '/call/payment?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url = app.config.get("BACKEND_URL") + '/cancel.html',
+    )
+    return checkout_session.url
+    return redirect(checkout_session.url)
 
 @app.route('/call/payment', methods=['GET'])
 def call_payment():
+    return redirect("https://jottedonline.com/organizer-dashboard")
+    return APIResponse.success("Plan updated successfully", 200)
     chk_ses_id = request.args.get('session_id')
     checkout_session = stripe.checkout.Session.retrieve(id=chk_ses_id)
     sub = stripe.Subscription.retrieve(id=checkout_session.subscription)
     if sub.status == "active":
         user = User.query.get(checkout_session.metadata["user_id"])
-        user.plan = {str(checkout_session.metadata["ins_type"]): { "c": 1, "s": int(checkout_session.metadata["students"]), "t": int(checkout_session.metadata.get("team")), "d": checkout_session.metadata["duration"]}}
+        user.plan = {str(checkout_session.metadata["ins_type"]): { "c": int(checkout_session.metadata["count"]), "s": int(checkout_session.metadata["students"]), "t": int(checkout_session.metadata.get("team")), "d": checkout_session.metadata["duration"]}}
         user.stripe_sub_id = checkout_session.subscription
         stripe.Subscription.modify(checkout_session.subscription, metadata=checkout_session.metadata)
         db.session.commit()
@@ -104,3 +119,41 @@ def call_payment():
     else:
         return APIResponse.success("Something went wrong with payment, Please contact technical support", 403)
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    event = None
+    payload = request.data
+    sig_header = request.headers['STRIPE_SIGNATURE']
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, app.config.get("STRIPE_ENDPOINT_SECRET")
+        )
+    except ValueError as e:
+        raise e
+    except stripe.error.SignatureVerificationError as e:
+        raise e
+
+    if event['type'] == 'customer.subscription.deleted':
+        stripe_cus_id = event['data']['object']['customer']
+        user = User.query.filter_by(stripe_cus_id = stripe_cus_id).first()
+        if user:
+            user.stripe_sub_id = None
+            user.plan = {"0": {"c": 1, "d": "m", "s": 10}}
+            db.session.commit()
+
+    elif event['type'] == 'customer.subscription.updated':
+        stripe_cus_id = event['data']['object']['customer']
+        user = User.query.filter_by(stripe_cus_id = stripe_cus_id).first()
+        if user:
+            if event['data']['object']['status'] == "active":
+                user.stripe_sub_id = event['data']['object']['id']
+                md = event['data']['object']['items']['data'][0]['price']['metadata']
+                qty = event['data']['object']['items']['data'][0]['quantity']
+                user.plan = {str(md["ins_type"]): {"c": int(qty), "d": md["recurring"], "s": int(md["students"]), "t": int(md.get("team", 0))}}
+                db.session.commit()
+            else:
+                user.plan = {"0": {"c": 1, "d": "m", "s": 10}}
+                db.session.commit()
+
+    return jsonify(success=True)
