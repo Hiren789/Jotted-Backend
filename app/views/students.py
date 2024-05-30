ab945b01fcefb489a586ea9541429a2be54335d0
DummyTummy123 <hiren@magentaconnect.com>
2024-05-30 10:02:36 -0400

    Fixed add_student

--- app/views/students.py
+++ app/views/students.py
@@ -23,8 +23,8 @@ def add_student():
         return APIResponse.error("User has no access to add student to this institute", 403)
     owneruser = User.query.get(ins.user_id)
     usedcnt = 0
-    for ins in Institute.query.filter_by(id=owneruser.id).all():
-        usedcnt += ins.get_students(cnt=True)
+    for ownins in Institute.query.filter_by(id=owneruser.id).all():
+        usedcnt += ownins.get_students(cnt=True)
     if usedcnt >= owneruser.plan[str(ins.ins_type)]["s"]:
         return APIResponse.error(f"User's current plan has no capacity to add new student", 400)
     cgexists = ins.cgexists(data["campus_id"], data["grade"])
